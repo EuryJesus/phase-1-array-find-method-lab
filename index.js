40203874085bbe71d82ed51d7f8fa9411c1e1595
EuryJesus <Eury0218@hotmail.com>
2023-04-13 23:22:52 -0400

    Adds index.js

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// code your solution here
+function superbowlWin(record) {
+    
+}
+function superbowlWin(array) {
+    array.result === 'W'
+    return '2015'
+    }
+    function superbowlWin(array){
+        for(let item of array){
+            if(item.result === "W"){
+                return item.year;
+            }
+        }
+    }
+    
